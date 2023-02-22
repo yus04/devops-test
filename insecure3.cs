1aea4beabadc00648ce9c50bbd31529357675348
Yu Saito <t-yusaito@microsoft.com>
2023-02-22 12:00:36 +0900

    create insecure3.cs

---
+++ insecure3.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    public sring GetSecret()
+    {
+        var secret = "12345678qwertyuiop";
+        return secret;
+    }
+}
